116861be5ad4541ae4d9ced54feb44ca323d8bac
Munira <munira@comparisure.co.za>
2023-04-30 19:32:19 +0200

    Staging inspections table

--- transforms/models/marts/crashes/dim_crashes__conditions.sql
+++ transforms/models/marts/crashes/dim_crashes__conditions.sql
@@ -9,7 +9,7 @@ with data as (
 select {{ dbt_utils.generate_surrogate_key(['trafficway_desc',
 'weather_condition_desc',
 'light_condition_desc',
-'road_surface_condition_desc']) }} as  conditions_id,
+'road_surface_condition_desc']) }} as  condition_id,
 Trafficway_Desc,
 Weather_condition_desc,
 Light_condition_desc,
--- transforms/models/marts/crashes/fact_crashes.sql
+++ transforms/models/marts/crashes/fact_crashes.sql
@@ -1,22 +1,6 @@
-with conditions as (
+with raw_data as (
     select *
-    from {{ref('dim_crashes__conditions')}}
-),
-reports as (
-      select *
-    from {{ref('dim_crashes__reports')}}  
-),
-results  as (
-      select *
-    from {{ref('dim_crashes__results')}}  
-),
-vehicles  as (
-      select *
-    from {{ref('dim_crashes__vehicles')}}  
-),
-raw_data as (
-    select *
-    from {{ref('stg__crashes')}}
+    from {{ ref('stg__crashes') }}
 )
 
 select {{ dbt_utils.generate_surrogate_key(['seq_num',
@@ -24,11 +8,14 @@ select {{ dbt_utils.generate_surrogate_key(['seq_num',
 'dot_number',
 'report_number',
 'report_date']) }} as  report_id,
-{{ dbt_utils.generate_surrogate_key(['vehicle_license_number','vehicle_id_number','report_seq_no','vehicle_license_state']) }} as vehicle_id,
+{{ dbt_utils.generate_surrogate_key(['vehicle_license_number',
+'vehicle_id_number',
+'report_seq_no',
+'vehicle_license_state']) }} as vehicle_id,
 {{ dbt_utils.generate_surrogate_key(['trafficway_desc',
 'weather_condition_desc',
 'light_condition_desc',
-'road_surface_condition_desc']) }} as  conditions_id,
+'road_surface_condition_desc']) }} as  condition_id,
 {{ dbt_utils.generate_surrogate_key(['hazmat_released',
 'tow_away',
 'time_weight',
@@ -36,4 +23,3 @@ select {{ dbt_utils.generate_surrogate_key(['seq_num',
 fatalities,
 injuries
 from raw_data
- 
