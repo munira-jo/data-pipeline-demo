23f6d7d70b37bf013e417e13a14ccd9ce9c92971
Munira <munira@comparisure.co.za>
2023-05-01 20:51:29 +0200

    Added dotenv to requirements

--- Ingestion/copy_files_into_postgres.sh
+++ Ingestion/copy_files_into_postgres.sh
@@ -4,4 +4,4 @@
 
 \copy violation FROM 'C:\\Users\Munira\Desktop\demo-pipeline\raw_data\CleanedData\2022Aug_Violation.csv' WITH (FORMAT CSV,HEADER);
 
-\copy census FROM 'C:\\Users\Munira\Desktop\demo-pipeline\raw_data\CleanedData\2022Aug_Violation.csv' WITH (FORMAT CSV,HEADER);
+\copy census FROM 'C:\\Users\Munira\Desktop\demo-pipeline\raw_data\CleanedData\FMCSA_CENSUS1_2022Aug.csv' WITH (FORMAT CSV,HEADER);
